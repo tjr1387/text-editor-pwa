4b191aae8b3f53d786d3dc130d24001dc8e0efeb
Hair_Trigg3r <tylerjreimer@gmail.com>
2023-02-25 12:18:07 -0500

    Fill out logic for IDBs GET method

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,21 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => console.error('putDb not implemented, at least, not properly');
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// GET all content from the DB
+export const getDb = async () => {
+    // Create connection
+    const contactDb = await openDB('contact', 1);
+    // Create (configured) transaction
+    const transaxn = contactDb.transaction('contact', 'readonly');
+    // Open object store
+    const store = transaxn.objectStore('contact');
+    // Get everything in the object store & set it to a variable
+    const request = store.getAll();
+    // Set resulting data to a variable & return it
+    const result = await request;
+    return result;
+};
 
 initdb();
